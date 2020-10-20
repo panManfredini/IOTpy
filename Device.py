d92c8f94b781060d379bfbc04019eacf935d4a66
panManfredini <pan.manfredini@gmail.com>
2020-10-20 20:54:16 +0200

    Device class

---
+++ Device.py
@@ -0,0 +1,72 @@
+import abc
+from prometheus_client import Gauge 
+
+class promVar():
+    def __init__(self,name,description=""):
+        self.name = name
+        self.value = None
+        self.prom = Gauge(name,description)
+    
+    def setValue(self,value):
+        self.value = value
+        self.prom.set(value)
+    
+    def getValue(self):
+        return self.value
+
+
+class Device(abc.ABC):
+    def __init__(self):
+        self.variables = dict()
+        self.init()
+    
+    def addVariable(self,name,description=""):
+        self.variables[name] = promVar(name,description)
+    
+    def getVariableValue(self, name):
+        if self.variables.get(name) != None:
+            return self.variables[name].getValue()
+        else: 
+            return None
+    
+    def setVariableValue(self, name, value):
+        if self.variables.get(name) != None:
+            self.variables[name].setValue(value)
+    
+    def hasVar(self,name):
+        return self.variables.get(name) != None
+    
+    @abc.abstractmethod
+    def init(self):
+        pass
+    @abc.abstractmethod
+    def loop(self):
+        pass
+    @abc.abstractmethod
+    def write(self, name, value):
+        """
+        Parameters
+        ----------
+        name : str
+            varibale name that has been requested 
+        value : any
+            variable value that has to be written
+        """
+        pass
+    @abc.abstractmethod
+    def cleanup(self):
+        pass
+
+
+listOfDevices = []
+
+def addDevice(device):
+    listOfDevices.append(device)
+
+def getDeviceWithVar(name):
+    device = None
+    for dev in listOfDevices:
+        if dev.hasVar(name) :
+            device = dev
+            break
+    return device
--- prompy.py
+++ prompy.py
@@ -1,10 +1,8 @@
 from flask import Flask, escape, request
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
-from prometheus_client import start_http_server, Gauge 
-
-g = Gauge('my_inprogress_requests', 'Description of gauge')
-g.set(4.2)   # Set to a given value
+from Device import listOfDevices
+import temp
 
 
 # Create my app
